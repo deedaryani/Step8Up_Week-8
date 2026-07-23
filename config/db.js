17d5ddebc1d97257c115c95d62e06f049426ae87
deedaryani <daryanideepanshu@gmail.com>
2026-07-23 23:56:26 +0100

    updated environment files to deploy it on aiven and render

--- config/db.js
+++ config/db.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : undefined
 });
 
 module.exports = pool;
